9b9a4872e4c27b4047be4b7f9424ad6884ebce0d
cnelson20 <cjnelson2004@gmail.com>
2022-05-24 19:37:29 +0100

    added redirect

--- app/main.js
+++ app/main.js
@@ -2,10 +2,35 @@
     const nunjucks = require('nunjucks');
     nunjucks.configure('templates', { autoescape: true });
 
-    module.exports.handleRequest = function (req, res) {
-        res.writeHead(200, {
-		    'Content-Type' : 'text/html'
-	    });
+    const split_url = function (string) {
+        let i = string.indexOf('?');
+        if (i !== -1) {
+            return [string.substring(0, i), string.substring(i + 1)];
+        } else {
+            return [string, ''];
+        }
+    }
+
+    const page404 = function (response) {
+        response.writeHead(404, {});
+        response.end("Page not found");
+    };
+
+    const home = function (req, res) {
+        res.writeHead(200, {});
 	    res.end(nunjucks.render('index.html', { name: 'Cameron' }));
+    };
+
+    module.exports.handleRequest = function (req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        switch (split_url(req.url)[0]) {
+            case '/':
+                home(req, res);
+                break;
+            default:
+                res.writeHead(301, {Location : '/'});
+                res.end();
+                break;
+        }
     }
 }());
